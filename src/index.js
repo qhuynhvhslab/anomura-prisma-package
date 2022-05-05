37c55c228debd6c819750cc684836de7eb6cf7b5
LAPTOP-QS1LABDN\quan6 <qhuynh@vhslab.com>
2022-05-04 23:30:20 -0400

    test

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-//import { Context } from "./index.d";
-
-const prisma = new PrismaClient();
-
-// export const createContext = async (): Promise<Context> => ({ prisma });
-export const createContext = async () => ({ prisma });
